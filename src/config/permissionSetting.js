bbca070f6fe1a51a49e38fd57a0a0cbeb8f55cf0
fhhadmin <1120777236@qq.com>
2019-04-01 13:47:15 +0800

    权限管理

--- src/config/permissionSetting.js
+++ src/config/permissionSetting.js
@@ -1,83 +1,94 @@
 export default {
   jurisdiction: [
     {
-      value: '工资管理',
+      value: '组织架构管理',
       expand: true,
-      title: '工资管理',
-      id: '工资管理',
+      title: '组织架构管理',
+      id: '组织架构管理',
+      children: [
+        {
+          value: '组织架构图',
+          title: '组织架构图',
+          id: '组织架构管理'
+        }
+      ]
+    },
+    {
+      value: '薪资管理',
+      expand: true,
+      title: '薪资管理',
+      id: '薪资管理',
       children: [
         {
           value: '薪酬管理',
           title: '薪酬管理',
-          id: '工资管理'
+          id: '薪资管理'
         },
         {
           value: '绩效考核',
           title: '绩效考核',
-          id: '工资管理'
+          id: '薪资管理'
         },
         {
           value: '考勤管理',
           title: '考勤管理',
-          id: '工资管理'
+          id: '薪资管理'
         }
       ]
     },
     {
-      value: '人事管理',
+      value: '人事档案管理',
       expand: true,
-      title: '人事管理',
-      id: '人事管理',
+      title: '人事档案管理',
+      id: '人事档案管理',
       children: [
         {
           value: '人员信息',
           title: '人员信息',
-          id: '人事管理'
+          id: '人事档案管理'
         },
         {
           value: '新聘员工',
           title: '新聘员工',
-          id: '人事管理'
-        }
-      ]
-    },
-    {
-      value: '档案管理',
-      expand: true,
-      title: '档案管理',
-      id: '档案管理',
-      children: [
+          id: '人事档案管理'
+        },
         {
           value: '基础档案',
           title: '基础档案',
-          id: '档案管理',
-          children: [
-            {
-              value: '基础档案',
-              title: '基础档案',
-              id: '档案管理'
-            },
-            {
-              value: '合同档案',
-              title: '合同档案',
-              id: '档案管理'
-            },
-            {
-              value: '培训档案',
-              title: '培训档案',
-              id: '档案管理'
-            }
-          ]
+          id: '人事档案管理'
         },
         {
           value: '合同档案',
           title: '合同档案',
-          id: '档案管理'
+          id: '人事档案管理'
         },
         {
           value: '培训档案',
           title: '培训档案',
-          id: '档案管理'
+          id: '人事档案管理'
+        }
+      ]
+    },
+    {
+      value: '报表统计',
+      expand: true,
+      title: '报表统计',
+      id: '报表统计',
+      children: [
+        {
+          value: '薪资明细',
+          title: '薪资明细',
+          id: '报表统计'
+        },
+        {
+          value: '培训历史',
+          title: '培训历史',
+          id: '报表统计'
+        },
+        {
+          value: '考勤统计',
+          title: '考勤统计',
+          id: '报表统计'
         }
       ]
     }
